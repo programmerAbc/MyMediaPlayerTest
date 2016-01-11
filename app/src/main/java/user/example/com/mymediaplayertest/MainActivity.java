5b0ddbfee9a143812b784e5c6298cb9c72c52069
FFT <2355379654@qq.com>
2016-01-11 16:22:29 +0800

    add some resource

--- app/src/main/java/user/example/com/mymediaplayertest/MainActivity.java
+++ app/src/main/java/user/example/com/mymediaplayertest/MainActivity.java
@@ -1,9 +1,8 @@
 package user.example.com.mymediaplayertest;
 
-import android.app.ActionBar;
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
