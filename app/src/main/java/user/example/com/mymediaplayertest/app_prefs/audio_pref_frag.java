63e6034a89837095ffa50747602fdbb8a4c5f96f
FFT <2355379654@qq.com>
2016-01-15 16:33:59 +0800

    wip

---
+++ app/src/main/java/user/example/com/mymediaplayertest/app_prefs/audio_pref_frag.java
@@ -0,0 +1,10 @@
+package user.example.com.mymediaplayertest.app_prefs;
+
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by user1 on 2016/1/15.
+ */
+public class audio_pref_frag extends PreferenceFragment{
+
+}
